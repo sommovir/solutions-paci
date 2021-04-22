54b7321de66c48443e32345eda2d70b749b8b3bf
Alessio <alessiopecilli@gmail.com>
2021-04-22 17:39:18 +0200

    Fix classi Advanced_Bulb

--- src/main/java/it/cnr/istc/solutions/paci/team3/Remote_Advanced_Bulb.java
+++ src/main/java/it/cnr/istc/solutions/paci/team3/Remote_Advanced_Bulb.java
@@ -9,15 +9,28 @@ package it.cnr.istc.solutions.paci.team3;
  *
  * @author Alessio
  */
-public class Remote_Advanced_Bulb extends Sensor{
+public class Remote_Advanced_Bulb extends Remote_Bulb{
+    
+private int colori[] = new int[3];
 
     public Remote_Advanced_Bulb(int id, int livello_batteria, String nome, boolean stato, Location luogo) {
         super(id, livello_batteria, nome, stato, luogo);
     }
-
-  
-
-   
+    
+    public void Change_Color(int[] c) {
+        for (int i = 0; i < 3; i++) {
+            this.colori[i] = c[i];
+        }
+    }
+    
+    public boolean isValidColor(int[] c){
+        for (int i = 0; i < 3; i++) {
+            if(c[i]>=0 && c[i]<=255){
+                return true;
+            }
+        }
+        return false;
+    }
     
     
 }

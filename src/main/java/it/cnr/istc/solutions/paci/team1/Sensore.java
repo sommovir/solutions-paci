f876e1b8835e7eadb54e6bbd02a43bdca74869d1
Diego <d1390dm@gmail.com>
2021-04-22 17:36:58 +0200

    final?

--- src/main/java/it/cnr/istc/solutions/paci/team1/Sensore.java
+++ src/main/java/it/cnr/istc/solutions/paci/team1/Sensore.java
@@ -9,7 +9,7 @@ package it.cnr.istc.solutions.paci.team1;
  *
  * @author d1390
  */
-public class Sensore {
+public abstract class Sensore {
     
     private int id;
     private float batteryLevel;
@@ -17,11 +17,45 @@ public class Sensore {
     private boolean powerStatus; // On/OFF
     
     public Sensore(){}
-    public Sensore(int id, float batteryLevel, Location location, boolean powerStatus ){
+    
+    public int getID(){
+    
+        return this.id;
+        
+    }
+    public float getBatteryLevel(){
+    
+        return this.batteryLevel;
+    
+    } 
+    public boolean getPowerStatus(){
+    
+        return this.powerStatus;
+        
+    }
+    public Location getLocation(){
+        
+        return this.location;
+        
+    }
+    
+    public void setID(int id){
     
         this.id = id;
+    
+    }   
+    public void setBatteryLevel(float batteryLevel){
+    
         this.batteryLevel = batteryLevel;
+    
+    }
+    public void setLocation(Location location){
+    
         this.location = location;
+    
+    }
+    public void setPowerStatus(boolean powerStatus){
+    
         this.powerStatus = powerStatus;
     
     }

20a6e9b7cab0003788a6f59044a1b8cff329de40
Diego <d1390dm@gmail.com>
2021-04-22 17:04:52 +0200

    init

---
+++ src/main/java/it/cnr/istc/solutions/paci/team1/SensorType/ADV_RemoteBulb.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.cnr.istc.solutions.paci.team1.SensorType;
+
+/**
+ *
+ * @author d1390
+ */
+public class ADV_RemoteBulb extends RemoteBulb{
+    
+}
---
+++ src/main/java/it/cnr/istc/solutions/paci/team1/SensorType/DoorLock.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.cnr.istc.solutions.paci.team1.SensorType;
+
+import it.cnr.istc.solutions.paci.team1.Sensore;
+
+/**
+ *
+ * @author d1390
+ */
+public class DoorLock extends Sensore {
+    
+}

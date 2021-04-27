d2d2fb496f54a75e4ebcdd1c428e0cfcc197a8a8
Diego <d1390dm@gmail.com>
2021-04-27 16:57:51 +0200

    final?

--- src/main/java/it/cnr/istc/solutions/paci/team1/SensorType/DoorLock.java
+++ src/main/java/it/cnr/istc/solutions/paci/team1/SensorType/DoorLock.java
@@ -10,42 +10,32 @@ import it.cnr.istc.solutions.paci.team1.Sensore;
 
 /**
  *
- * @author d1390
+ * @author Spirito
  */
 public class DoorLock extends Sensore {
     
+    /*
     private boolean contact;    //false non contatto true contatto.
     private String message;
+    */
     
-    public DoorLock (boolean c,String m,boolean contact,String message,int  id, Location location, float batteryLevel, boolean powerStatus){
+    public DoorLock (/*boolean c,String m,boolean contact,String message,*/int  id, Location location, float batteryLevel, boolean powerStatus){
     
         super(id, batteryLevel, location, powerStatus);
      
-        this.contact = c;
-        this.message = m;
-                
+        //this.contact = c;
+        //this.message = m;
     
     }
     
+    //Spirito non sarebbe meglioe evitare di istanziare come attributi contact e messages? passi conctact come parametro e message fai return e basta?
+    //ho cambiato... :)
     
-    public String status () {
+    public String getStatus (boolean contact) {
     
-    if(contact == false){
+    if(!contact) return "unlock";
+        else return "lock";        
     
-        message = "unlock";
-        
-    }
-    
-    else{
-    
-        message = "lock";
-    
-    }
-    
-    return message;
-    
-    }
-    
-    
+    }        
     
 }

7661a527b8a3f9c7d8e0230de2c8cbd013e1df78
Diego <d1390dm@gmail.com>
2021-04-27 16:57:58 +0200

    final?

--- src/main/java/it/cnr/istc/solutions/paci/team1/SensorType/PIR.java
+++ src/main/java/it/cnr/istc/solutions/paci/team1/SensorType/PIR.java
@@ -5,6 +5,7 @@
  */
 package it.cnr.istc.solutions.paci.team1.SensorType;
 
+import it.cnr.istc.solutions.paci.team1.Location;
 import it.cnr.istc.solutions.paci.team1.Sensore;
 
 /**
@@ -12,5 +13,20 @@ import it.cnr.istc.solutions.paci.team1.Sensore;
  * @author d1390
  */
 public class PIR extends Sensore{
+    
+    public PIR (int  id, Location location, float batteryLevel, boolean powerStatus){
+    
+        super(id, batteryLevel, location, powerStatus);           
+    
+    }
+    
+    public boolean getStatus(boolean ritorno){
+    
+        if(ritorno)
+            return true;
+        else  
+            return false;
+    }
+    
     
 }

ac4877720197b521516a543b60bcdb66a1766bcc
Diego <d1390dm@gmail.com>
2021-04-27 16:58:04 +0200

    final?

--- src/main/java/it/cnr/istc/solutions/paci/team1/SensorType/PowerPlug.java
+++ src/main/java/it/cnr/istc/solutions/paci/team1/SensorType/PowerPlug.java
@@ -5,6 +5,7 @@
  */
 package it.cnr.istc.solutions.paci.team1.SensorType;
 
+import it.cnr.istc.solutions.paci.team1.Location;
 import it.cnr.istc.solutions.paci.team1.Sensore;
 
 /**
@@ -12,5 +13,26 @@ import it.cnr.istc.solutions.paci.team1.Sensore;
  * @author d1390
  */
 public class PowerPlug extends Sensore{
+    
+    private float corrente;
+    
+    public PowerPlug (int  id, Location location, float batteryLevel, boolean powerStatus){
+    
+        super(id, batteryLevel, location, powerStatus);
+        this.corrente = corrente;
+                
+    }
+    
+    public void SetPower(boolean Pswitch){
+    
+        this.powerStatus = Pswitch;
+    
+    }
+    
+    public float getCorrente(){
+        
+        return this.corrente;
+        
+    }
     
 }
